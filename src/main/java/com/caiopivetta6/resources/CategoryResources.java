d104f97fdee0b15073b452045fcb52b6a91a9b48
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-05 13:51:56 +0100

    Class Category inserted

--- src/main/java/com/caiopivetta6/resources/CategoryResources.java
+++ src/main/java/com/caiopivetta6/resources/CategoryResources.java
@@ -1,5 +1,29 @@
 package com.caiopivetta6.resources;
 
-public class CategoryResources {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caiopivetta6.domain.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+	
+	@GetMapping
+	public List<Category> listCategory () {
+		
+		Category category1 = new Category(1, "Informatica");
+		Category category2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(category1);
+		list.add(category2);
+		
+		return list;
+	}
+	
 }
